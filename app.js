f0af5188006fbbc5148d6f95e49915268d1227ff
VinceLee9527 <vlee9527@gmail.com>
2021-09-05 17:12:28 +0800

    Added contact/portfolio/about pages

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
